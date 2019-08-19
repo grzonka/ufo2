28ce369c6c021ca9b29d810a12689cf39d81ef2e
Florian Grzonka <florian.grzonka@campus.lmu.de>
2019-08-19 23:55:33 +0200

    changed resolution to fit demands of 160x144p. added movable character.

--- core/src/net/grzonka/ufo2/Ufo2.java
+++ core/src/net/grzonka/ufo2/Ufo2.java
@@ -3,6 +3,7 @@ package net.grzonka.ufo2;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,71 +12,78 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Ufo2 extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture megaMan;
-	BitmapFont font;
-	Sprite sprite;
+
+  SpriteBatch batch;
+  Texture background;
+  Texture smallBoy;
+  BitmapFont font;
+  Sprite sprite;
+  Sprite backgroundSprite;
 
 	/*
 	Other functions that can be implemented are:
 	resize()
 	pause()
 	resume()
-	 */
+	*/
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		megaMan = new Texture(Gdx.files.internal("Mega_man.png"));
-		font = new BitmapFont();
-		font.setColor(Color.BLACK);
-		sprite = new Sprite(megaMan);
-	}
+  @Override
+  public void create() {
+    batch = new SpriteBatch();
+    background = new Texture(Gdx.files.internal("background.png"));
+    smallBoy = new Texture(Gdx.files.internal("small_boy.png"));
+    font = new BitmapFont();
+    font.setColor(Color.BLACK);
+    backgroundSprite = new Sprite(background);
+    sprite = new Sprite(smallBoy);
+  }
 
-	// render() is called each frame. (can be thought of as an event loop)
-	@Override
-	public void render() {
-		// gdx.gl is exposing OpenGL functionality
-		Gdx.gl.glClearColor(255, 255, 255, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(-1f);
-			else
-				sprite.translateX(-10.0f);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(1f);
-			else
-				sprite.translateX(10.0f);
-		}
+  // render() is called each frame. (can be thought of as an event loop)
+  @Override
+  public void render() {
+    // gdx.gl is exposing OpenGL functionality
+    Gdx.gl.glClearColor(255, 255, 255, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
-		//font.draw(batch, "Hello, World", 200, 200);
-		//batch.draw(img, 0, 0);
-		sprite.draw(batch);
-		batch.end();
-	}
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+      sprite.translateX(-3f);
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+      sprite.translateX(3f);
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      sprite.translateY(3f);
+    }
+    if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+      sprite.translateY(-3f);
+    }
 
-	@Override
-	public void resize(int width, int height){
-	}
+    batch.begin();
+    //font.draw(batch, "Hello, World", 200, 200);
+    //batch.draw(img, 0, 0);
+    backgroundSprite.draw(batch);
+    sprite.draw(batch);
+    batch.end();
+  }
 
-	@Override
-	public void pause(){
-	}
+  @Override
+  public void resize(int width, int height) {
+  }
 
-	@Override
-	public void resume(){
-	}
+  @Override
+  public void pause() {
+  }
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-		//img.dispose();
-		megaMan.dispose();
-		font.dispose();
-	}
+  @Override
+  public void resume() {
+  }
+
+  @Override
+  public void dispose() {
+    batch.dispose();
+    //img.dispose();
+    background.dispose();
+    smallBoy.dispose();
+    font.dispose();
+  }
 }
--- desktop/src/net/grzonka/ufo2/desktop/DesktopLauncher.java
+++ desktop/src/net/grzonka/ufo2/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import net.grzonka.ufo2.Ufo2;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Ufo2 someTitle";
+		config.useGL30 = true;
+		config.width = 160;
+		config.height = 144;
+
 		new LwjglApplication(new Ufo2(), config);
 	}
 }
