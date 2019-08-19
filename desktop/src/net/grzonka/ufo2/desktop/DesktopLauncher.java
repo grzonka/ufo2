ee5ee24a1088c53c84b82a66a3093f76f2ddee66
Florian Grzonka <florian.grzonka@campus.lmu.de>
2019-08-19 19:38:30 +0200

    added e,mpty Ãlibgdx project.

---
+++ desktop/src/net/grzonka/ufo2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.grzonka.ufo2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.grzonka.ufo2.Ufo2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ufo2(), config);
+	}
+}
