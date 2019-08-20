0ec804cfb3a597e1ad5ecc90dd02b4f2f2e74e87
Florian Grzonka <florian.grzonka@campus.lmu.de>
2019-08-20 15:41:03 +0200

    implemented camera for moving around in space.

--- core/src/net/grzonka/ufo2/Ufo2.java
+++ core/src/net/grzonka/ufo2/Ufo2.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -19,14 +21,22 @@ public class Ufo2 extends ApplicationAdapter {
   BitmapFont font;
   Sprite sprite;
   Sprite backgroundSprite;
+  Camera camera;
 
   @Override
   public void create() {
     batch = new SpriteBatch();
-    background = new Texture(Gdx.files.internal("background.png"));
+    background = new Texture(Gdx.files.internal("background_10_wide.png"));
     smallBoy = new Texture(Gdx.files.internal("small_boy_transparent.png"));
     backgroundSprite = new Sprite(background);
     sprite = new Sprite(smallBoy);
+    camera = new OrthographicCamera(160,144);
+    // moves camera to be centered that the beginning of the background is in place
+    camera.translate(80f,72f,0f);
+
+    System.out.println("MOVE RIGHT!");
+    // TODO: integrate box2d.
+
   }
 
   // render() is called each frame. (can be thought of as an event loop)
@@ -52,9 +62,19 @@ public class Ufo2 extends ApplicationAdapter {
       sprite.rotate(4f);
     }
 
+    // System.out.println("Sprites location is: (" + sprite.getX() + "," + sprite.getY() + ").");
+    // System.out.println(camera.combined);
+
+    // camera setup:
+    batch.setProjectionMatrix(camera.combined);
+    // continous update not bound to movement yet, just a sidescroller for now
+    camera.translate(0.3f,0f,0f);
+    camera.update();
+
     batch.begin();
     backgroundSprite.draw(batch);
     sprite.draw(batch);
+
     batch.end();
   }
 
