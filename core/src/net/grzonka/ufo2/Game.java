a45fd682d6b4267f860dfa00a7f9a3efe2872724
vintieu <you@example.com>
2019-08-26 02:09:37 +0200

    added functionality to prevent the game from starting if not started

--- core/src/net/grzonka/ufo2/Game.java
+++ core/src/net/grzonka/ufo2/Game.java
@@ -17,6 +17,8 @@ import net.grzonka.ufo2.model.B2DVars;
 
 public class Game extends ApplicationAdapter {
 
+  public boolean gameHasStarted = false;
+
   public static final String TITLE = "Ufo2 - early release.";
   public static final int V_WIDTH = 160;
   public static final int V_HEIGHT = 144;
@@ -91,37 +93,40 @@ public class Game extends ApplicationAdapter {
 
 
     }
+    if(gameHasStarted) {
     spriteBatch.begin();
     scoreFont.setColor(0f, 0f, 0f, 1f);
     scoreFont.getData().setScale(0.1f);
 
-    ShapeRenderer shapeRenderer = new ShapeRenderer();
-    shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-    shapeRenderer.setProjectionMatrix(spriteBatch.getProjectionMatrix());
-    shapeRenderer.identity();
-    shapeRenderer.setColor(Color.LIGHT_GRAY);
-    shapeRenderer.rect(74 / B2DVars.PPM, 136 / B2DVars.PPM, 80 / B2DVars.PPM, 5 / B2DVars.PPM);
-    if((health <= MAX_HEALTH/4) && ((health /60) %3 == 0)) { //blinking effect when health drops below 25 %
-    shapeRenderer.setColor(Color.LIGHT_GRAY);
-    }else {shapeRenderer.setColor(Color.BLACK);}
-    shapeRenderer
-            .rect(74 / B2DVars.PPM, 136 / B2DVars.PPM, (health / 100f) / B2DVars.PPM, 5 / B2DVars.PPM);
-    if (health >= 0) {
-      health -= 6;
-    } else {
-      System.out.println("GAME Should end here");
-    }
-    shapeRenderer.end();
-    spriteBatch.end();
+      ShapeRenderer shapeRenderer = new ShapeRenderer();
+      shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+      shapeRenderer.setProjectionMatrix(spriteBatch.getProjectionMatrix());
+      shapeRenderer.identity();
+      shapeRenderer.setColor(Color.LIGHT_GRAY);
+      shapeRenderer.rect(74 / B2DVars.PPM, 136 / B2DVars.PPM, 80 / B2DVars.PPM, 5 / B2DVars.PPM);
+      if ((health <= MAX_HEALTH / 4) && ((health / 60) % 3 == 0)) { //blinking effect when health drops below 25 %
+        shapeRenderer.setColor(Color.LIGHT_GRAY);
+      } else {
+        shapeRenderer.setColor(Color.BLACK);
+      }
+      shapeRenderer
+              .rect(74 / B2DVars.PPM, 136 / B2DVars.PPM, (health / 100f) / B2DVars.PPM, 5 / B2DVars.PPM);
+      if (health >= 0 &&gameHasStarted) {
+        increaseHealth(-6);
+      } else {
+        System.out.println("GAME Should end here");
+      }
+      shapeRenderer.end();
+      spriteBatch.end();
 
-    spriteBatch.begin();
-    scoreFont.draw(spriteBatch, scoreDiplayed, 0.5f, 14);
-    spriteBatch.end();
-
+      spriteBatch.begin();
+      scoreFont.draw(spriteBatch, scoreDiplayed, 0.5f, 14);
+      spriteBatch.end();
+    }else{
     spriteBatch.begin();
     //TODO remove this when game hast started
     spriteBatch.draw(startScreenSprite,0,0,160f/B2DVars.PPM,144f/B2DVars.PPM);
-    spriteBatch.end();
+    spriteBatch.end();}
   }
 
   public static void increaseHealth(int hpIncrease) {
@@ -137,6 +142,10 @@ public class Game extends ApplicationAdapter {
 
   @Override
   public void resize(int width, int height) {
+  }
+
+  public void gameHasStartedHud(){
+    this.gameHasStarted = true;
   }
 
   @Override
